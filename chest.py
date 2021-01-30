b2ccff7a617d8e2cee4d6945496b6ef4c5e24eff
Qwerty042 <28468486+Qwerty042@users.noreply.github.com>
2021-01-30 19:07:15 +1300

    chest unlocking working yay

--- chest.py
+++ chest.py
@@ -2,18 +2,119 @@ import pygame as pg
 import os
 
 class Chest(pg.sprite.Sprite):
-  def __init__(self, width, height, pos):
+  def __init__(self, width, height, pos, screen_width, screen_height):
     pg.sprite.Sprite.__init__(self)
+    self.font = pg.font.SysFont(None, 128)
+    self.font.set_bold(True)
     self.open_image = pg.transform.smoothscale(pg.image.load(os.path.join("Assets", "temp_chest_open_1.png")), (width,height)).convert_alpha()
     self.closed_image = pg.transform.smoothscale(pg.image.load(os.path.join("Assets", "temp_chest_closed_1.png")), (width,height)).convert_alpha()
     self.image = self.closed_image
     self.rect = self.image.get_rect()
-    self.rect.center = pos
-    self.closed = True
+    self.pos = pos
+    self.rect.center = self.pos
+    self.screen_width = screen_width
+    self.screen_height = screen_height
+    self.is_closed = True
+    self.is_locked = True
+    self.is_interacting = False
+    self.is_digit_pressed = False
+    self.digits = [None, None, None, None]
+    self.passcode = [1, 2, 3, 4]
 
   def interact(self):
-    self.closed = not self.closed
-    if self.closed: self.image = self.closed_image
-    else: self.image = self.open_image
+    self.is_interacting = not self.is_interacting
+    if not self.is_locked:
+      self.is_closed = not self.is_closed
+      if self.is_closed: self.image = self.closed_image
+      else: self.image = self.open_image
+    else:
+      if not self.is_interacting:
+        self.digits = [None, None, None, None]
+        self.image = self.closed_image
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
 
-    
+  def _draw_digits(self):
+    block_height = 300
+    block_width = 150
+    block_offset = (50,50)
+    block_offset_x_step = self.rect.width//5
+    self.image
+    for digit in self.digits:
+      if digit == None:
+        digit_str = '_'
+      else:
+        digit_str = str(digit)
+      rendered_digit = self.font.render(digit_str, True, (200,200,200))
+      digit_width, digit_height = rendered_digit.get_size()
+      rendered_block = pg.Surface((block_width,block_height))
+      rendered_block.fill((20,60,80))
+      digit_offset = (block_width//2 - digit_width//2, block_height//2 - digit_height//2)
+      rendered_block.blit(rendered_digit, digit_offset)
+      self.image.blit(rendered_block, block_offset)
+      block_offset = (block_offset[0] + block_offset_x_step, block_offset[1])
+
+
+
+  def update(self):
+    if self.is_interacting and self.is_locked:
+      self.image = pg.Surface((self.screen_width - 200, self.screen_height - 200))
+      self.image.fill((79, 83, 97))
+      self.rect = self.image.get_rect()
+      self.rect.center = (self.screen_width//2, self.screen_height//2)
+      
+      keys = pg.key.get_pressed()
+      if not self.is_digit_pressed:
+        if keys[pg.K_0]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 0
+        elif keys[pg.K_1]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 1
+        elif keys[pg.K_2]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 2
+        elif keys[pg.K_3]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 3
+        elif keys[pg.K_4]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 4
+        elif keys[pg.K_5]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 5
+        elif keys[pg.K_6]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 6
+        elif keys[pg.K_7]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 7
+        elif keys[pg.K_8]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 8
+        elif keys[pg.K_9]:
+          self.is_digit_pressed = True
+          self.digits[self.digits.index(None)] = 9
+      elif not (keys[pg.K_0]
+                or keys[pg.K_1]
+                or keys[pg.K_2]
+                or keys[pg.K_3]
+                or keys[pg.K_4]
+                or keys[pg.K_5]
+                or keys[pg.K_6]
+                or keys[pg.K_7]
+                or keys[pg.K_8]
+                or keys[pg.K_9]): self.is_digit_pressed = False
+
+      self._draw_digits()
+
+      if self.digits.count(None) == 0:
+        if self.digits == self.passcode:
+          self.is_locked = False
+          self.is_closed = False
+          self.image = self.closed_image
+          self.rect = self.image.get_rect()
+          self.rect.center = self.pos
+        self.interact()
+      
+      
--- game.py
+++ game.py
@@ -26,7 +26,7 @@ class MainGame:
     self.island = Island(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
     self.player = Player(566//8, 566//8, (200, 200)) # I like me some magic numbers nom nom
     self.paper = Paper(self.SCREEN_WIDTH, 120, (0, self.SCREEN_HEIGHT - 120))
-    self.chest = Chest(642//8, 683//8, (400, 400)) # more yummy magic numbers nom nom nom
+    self.chest = Chest(642//8, 683//8, (400, 400), self.SCREEN_WIDTH, self.SCREEN_HEIGHT) # more yummy magic numbers nom nom nom
 
     # list of all the sprite objects to be drawn
     # first in list is drawn first so will be underneath evertthing else
@@ -43,20 +43,24 @@ class MainGame:
     # add dank game logic here
     
     keys=pg.key.get_pressed()
-    if keys[pg.K_a]:
-      self.player.rotate(1)
-    if keys[pg.K_d]:
-      self.player.rotate(-1)
-    if keys[pg.K_w]:
-      self.player.move(1, self.island.mask, self.chest)
-    if keys[pg.K_s]:
-      self.player.move(-1, self.island.mask, self.chest)
+    if not self.chest.is_interacting:
+      if keys[pg.K_a]:
+        self.player.rotate(1)
+      if keys[pg.K_d]:
+        self.player.rotate(-1)
+      if keys[pg.K_w]:
+        self.player.move(1, self.island.mask, self.chest)
+      if keys[pg.K_s]:
+        self.player.move(-1, self.island.mask, self.chest)
     # if keys[pg.K_e]:
 
     for event in events:
       if (event.type == pg.KEYDOWN) and (event.key == pg.K_e):
-        player_to_chest_dist = abs(math.sqrt((self.player.rect.centerx - self.chest.rect.centerx)**2 + (self.player.rect.centery - self.chest.rect.centery)**2))
-        if player_to_chest_dist < 100:
+        if not self.chest.is_interacting:
+          player_to_chest_dist = abs(math.sqrt((self.player.rect.centerx - self.chest.rect.centerx)**2 + (self.player.rect.centery - self.chest.rect.centery)**2))
+          if player_to_chest_dist < 100:
+            self.chest.interact()
+        else:
           self.chest.interact()
 
 
